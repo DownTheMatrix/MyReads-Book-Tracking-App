a7ce21b04702afe4947d58a76ffb914c3a37e4d7
Bruno Mazza <suija87@gmail.com>
2018-07-26 15:24:35 +0200

    Rename to moveTo option value to avoid bugs

--- src/components/ShelfUpdate.js
+++ src/components/ShelfUpdate.js
@@ -16,7 +16,7 @@ class ShelfUpdate extends Component {
       <div className="book-shelf-changer">
         <select onChange={( event ) => arrangeShelf(book, event.target.value )}
           defaultValue={ currentShelf }>
-          <option value="none" disabled>Move to...</option>
+          <option value="moveTo" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
